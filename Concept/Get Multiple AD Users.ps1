99707fc325095a7307d8fa15660e4fd6d2119244
Trevor Rousseau <trousseau@gmail.com>
2022-08-19 10:51:01 -0500

    Updates to scripts.

--- Concept/Get Multiple AD Users.ps1
+++ Concept/Get Multiple AD Users.ps1
@@ -1,1 +1,4 @@
+# Gets all AD attributes from all users in the domain.
+# Warning: this can be a lengthy process.
+# Returns all data to a CSV file.
 Get-ADUser -Filter * -Properties * | Export-Csv "C:\Temp\Get Multiple AD Users.csv" -NoTypeInformation
