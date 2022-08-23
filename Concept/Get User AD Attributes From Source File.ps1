f37f8298d08a92acff0735b69b1e530fc2b83211
Trevor Rousseau <tsrousseau@cenlar.com>
2022-08-23 07:55:05 -0500

    Script updates and adding source file examples.

--- Concept/Get User AD Attributes From Source File.ps1
+++ Concept/Get User AD Attributes From Source File.ps1
@@ -1,9 +1,7 @@
 Import-Module ActiveDirectory
-$UserAttributesList = @()
+Remove-Item "C:\Temp\Results - AD User from Source File.csv"
 
-# Iterate over list to get ad user attributes filter by employee id and store result
-Import-CSV C:\Temp\ADUsers.csv | ForEach-Object { 
-Get-ADUser  -Filter "username -eq $($_.username)" -Properties SamAccountName, Name, Mail| Select-Object samaccountname, name, mail
+$userList = import-csv "C:\Temp\Source - AD Users List.csv"
+ForEach($User in $userList){
+     Get-ADUser -Identity $user.UserName -Properties * | Select-Object SamAccountName, Mail, CN, EmployeeID | Export-Csv -Append "C:\Temp\Results - AD User from Source File.csv" -NoTypeInformation
 }
-
-$UserAttributesList | Export-csv "C:\Temp\Get User AD Attributes From Source File.csv" -Append -NoTypeInformation
