bede314d02fbd0dc08b6f7edd11fbe76696d3731
Trevor Rousseau <trousseau@gmail.com>
2022-09-26 14:37:38 -0500

    Updates.

--- Concept/Get User AD Attributes From Source File.ps1
+++ Concept/Get User AD Attributes From Source File.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-Write-Host "Updating attributes from CSV file.  Please stand by..." -ForegroundColor Cyan
+Write-Host "Retrieving attributes from username input CSV file.  Please stand by..." -ForegroundColor Cyan
 
 # Insert a start date and timestamp.
 $dateTime = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
@@ -9,11 +9,11 @@ Import-Module ActiveDirectory
 
 $userList = import-csv ".\Source - AD Users List.csv"
 ForEach($User in $userList){
-     Get-ADUser -Identity $user.SamAccountName -Properties * | Select-Object SamAccountName, Mail, CN, EmployeeID | Export-Csv -Append "C:\Temp\Results - AD User from Source File.csv" -NoTypeInformation
+     Get-ADUser -Identity $user.SamAccountName -Properties * | Select-Object SamAccountName, userAccountControl, CN | Export-Csv -Append "C:\Temp\Results - AD User from Source File.csv" -NoTypeInformation
 }
 
 Write-Host "Results retrieved.  Process finished." -ForegroundColor Green
 
 # Insert an end date and timestamp.
 $dateTime = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
-Write-Host "$dateTime" -ForegroundColor Cyan
+Write-Host "$dateTime" -ForegroundColor Yellow
--- Development/Get Specified Attributes of All AD Users.ps1
+++ Development/Get Specified Attributes of All AD Users.ps1
@@ -9,7 +9,7 @@ $dateTime = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
 Write-Host "$dateTime" -ForegroundColor Yellow
 
 # Returns specified fields for all AD users in the domain.
-Get-ADUser -Filter * -Properties SamAccountName, HomePhone, OfficePhone | Select-Object SamAccountName, HomePhone, OfficePhone | Export-Csv "C:\Temp\Results - All Users and Only Specified Fields in Domain.csv" -NoTypeInformation -Append
+Get-ADUser -Filter * -Properties SamAccountName, CN | Select-Object SamAccountName, CN | Export-Csv "C:\Temp\Results - All Users and Only Specified Fields in Domain.csv" -NoTypeInformation -Append
 
 # Add date and time to exported file.
 $filenameFormat = "Results - " + (Get-Date -Format "MM-dd-yyyy HH-mm-ss") + " - All Users and Only Specified Fields in Domain.csv"
@@ -19,4 +19,4 @@ Write-Host "Results retrieved.  Process finished." -ForegroundColor Green
 
 # Insert an end date and timestamp.
 $dateTime = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
-Write-Host "$dateTime" -ForegroundColor Cyan
+Write-Host "$dateTime" -ForegroundColor Yellow
